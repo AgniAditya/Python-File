12de936b663d6c4fce1b062e77fdaddec2972c05
AgniAditya <agniaditya05@gmail.com>
2024-04-24 00:44:27 +0530

    First commit

--- Image-Viewer/Image_viewer.py
+++ Image-Viewer/Image_viewer.py
@@ -3,13 +3,35 @@ from PIL import Image,ImageTk
 
 window=Tk()
 window.title("Image Viewer")
-window.iconbitmap("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/multimediaphotoviewer_94421.ico")
+window.iconbitmap("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/multimediaphotoviewer_94421.ico")
 
-img_1=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/aidtya-01.jpg"))
-lab=Label(image=img_1,width=1118,height=800)
-lab.pack()
+img_1=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/aidtya-01.jpg"))
+img_2=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/space-01.jpg"))
+img_3=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/space-02.jpeg"))
+img_4=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/space-03.jpeg"))
+img_5=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/space-04.jpeg"))
+img_6=ImageTk.PhotoImage(Image.open("C:/Users/agnih/OneDrive/Desktop/python/Tkinter/Image-Viewer/space-05.jpeg"))
 
+global image_list
+image_list=[img_1,img_2,img_3,img_4,img_5,img_6]
+
+lab=Label(image=img_1,width=500,height=500)
+lab.grid(columnspan=3)
+
+def forward(img_no):
+    global lab
+    global forward_button
+    global backward_button
+    lab.grid_forget()
+    # for i in image_list:
+    lab1=Label(window,image=image_list[img_no-1],width=500,height=500)
+    lab1.grid(row=0,column=0,columnspan=3)
+
+backward_button=Button(window,text="<<")
 exit_button=Button(window,text="Exit",command=exit)
-exit_button.pack()
+forward_button=Button(window,text=">>",command=lambda:forward(2))
+forward_button.grid(row=1,column=2)
+exit_button.grid(row=1,column=1)
+backward_button.grid(row=1,column=0)
 
 window.mainloop()
