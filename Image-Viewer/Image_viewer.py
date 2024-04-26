f1fc87e6d290c3939291aea82bbabc5bcb57db93
AgniAditya <agniaditya05@gmail.com>
2024-04-27 01:55:17 +0530

    First Commit

--- Image-Viewer/Image_viewer.py
+++ Image-Viewer/Image_viewer.py
@@ -18,14 +18,22 @@ image_list=[img_1,img_2,img_3,img_4,img_5,img_6]
 lab=Label(image=img_1,width=500,height=500)
 lab.grid(columnspan=3)
 
+global bar
+bar=Label(window,text="image 0 of "+str(len(image_list)-1),bd=1,relief=SUNKEN)
+bar.grid(row=2,column=1,pady=10,sticky=W+E)
+
 def forward(img_no):
     global lab
     global forward_button
     global backward_button
+    global bar
+    bar.grid_forget()
     lab.grid_forget() 
     lab1=Label(window,image=image_list[img_no],width=500,height=500)
     forward_button=Button(window,text=">>",command=lambda:forward(img_no+1))
     backward_button=Button(window,text="<<",command=lambda:backward(img_no-1))
+    bar=Label(window,text="image "+str(img_no)+" of "+str(len(image_list)-1),bd=1,relief=SUNKEN)
+    bar.grid(row=2,column=1,sticky=W+E,pady=10)
     if img_no==5:
         forward_button=Button(window,text=">>",state=DISABLED)
     backward_button.grid(row=1,column=0)
@@ -36,10 +44,14 @@ def backward(img_no):
     global lab
     global forward_button
     global backward_button
+    global bar
+    bar.grid_forget()
     lab.grid_forget()
     lab=Label(window,image=image_list[img_no],width=500,height=500)
     forward_button=Button(window,text=">>",command=lambda:forward(img_no+1))
     backward_button=Button(window,text="<<",command=lambda:backward(img_no-1))
+    bar=Label(window,text="image "+str(img_no)+" of "+str(len(image_list)-1),bd=1,relief=SUNKEN)
+    bar.grid(row=2,column=1,sticky=W+E,pady=10)
     if img_no==0:
         backward_button=Button(window,text="<<",state=DISABLED)
     backward_button.grid(row=1,column=0)
@@ -52,5 +64,6 @@ forward_button=Button(window,text=">>",command=lambda:forward(1))
 forward_button.grid(row=1,column=2)
 exit_button.grid(row=1,column=1)
 backward_button.grid(row=1,column=0)
+
 
 window.mainloop()
